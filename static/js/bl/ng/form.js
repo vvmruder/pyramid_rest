02746dc4d39adc9db46456b6d70a4a86467f8201
Karsten Deininger <karsten.deininger@bl.ch>
2016-07-05 16:21:41 +0200

    Add form directive prototype

---
+++ static/js/bl/ng/form.js
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) 2012 - 2016, GIS-Fachstelle des Amtes für Geoinformation des Kantons Basel-Landschaft
+ * All rights reserved.
+ *
+ * This program is free software and completes the GeoMapFish License for the geoview.bl.ch specific
+ * parts of the code. You can redistribute it and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial
+ * portions of the Software.
+ *
+ * Created by Karsten Deininger on 05.07.16.
+ */
+
+angular.module('bl.ng.rest.form', ['bl.ng.rest.model', 'bl.ng.rest.field'])
+
+.controller('blNgRestFormCtrl', ['$scope', 'BlRestModel', function($scope, BlRestModel) {
+    $scope.model = {};
+    BlRestModel.query($scope.src + '/model.json').then(
+        function(response) {
+            $scope.model = response.data;
+        },
+        function(response) {
+            console.log(response.data || 'Model query failed.');
+        }
+    );
+}])
+
+.directive('blNgRestForm', function() {
+    return {
+        restrict: 'E',
+        scope: {
+            src: '@'
+        },
+        template: '<p ng-repeat="field in model.columns">{{field.header}}</p>'
+    }
+});
